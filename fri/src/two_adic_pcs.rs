f888f9014203e321371b7088cac0569769b2fb77
olegfomenko <oleg.fomenko2002@gmail.com>
2025-01-15 14:12:38 +0200

    adding text to the assertion in twi addic pcs get_evaluations_on_domain

--- fri/src/two_adic_pcs.rs
+++ fri/src/two_adic_pcs.rs
@@ -183,7 +183,7 @@ where
         // todo: handle extrapolation for LDEs we don't have
         assert_eq!(domain.shift, Val::GENERATOR);
         let lde = self.mmcs.get_matrices(prover_data)[idx];
-        assert!(lde.height() >= domain.size());
+        assert!(lde.height() >= domain.size(), "invalid data size {} less then {}", lde.height(), domain.size());
         lde.split_rows(domain.size()).0.bit_reverse_rows()
     }
 
